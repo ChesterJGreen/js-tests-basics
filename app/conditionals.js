2d18a665d147d33ecb566fbef8f1971625334ff4
cjgreenie <seejarom@gmail.com>
2021-06-28 17:22:18 -0600

    working

--- app/conditionals.js
+++ app/conditionals.js
@@ -1,7 +1,12 @@
 //1. Given two numbers, write a function that will return  the larger number
 
 function largerNum(num1, num2) {
-    // you code here
+    if (num1 > num2) {
+        return num1
+    }
+    else {
+        return num2
+    }
 
 }
 
@@ -14,9 +19,18 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
-
+    let x = score / possible;
+    if (x >= 0.9) {
+        return "A"
+    }
+    else if (x >= 0.8) {
+        return "B"
+    } else if (x >= 0.7) {
+        return "C"
+    } else if (x >= 0.6) {
+        return "D"
+    } else { return "F" }
 }
-
 
 // --------------------------------------------
 
@@ -24,18 +38,32 @@ function testGrader(score, possible) {
 // 3. Given an integer that represents an hour in the day (1 - 24), write a function that returns "good morning", "good afternoon", "good evening", or "good night" based off of what hour it is. You'll need to think in military time.
 //Morning is between 5am and 11am (5 - 11)
 //afternoon is between 12pm and 5pm (12 - 17)
-//evening is between 6pm and 9pm (18 - 21)
+//evening is between 6m and 9pm (18 - 21)
 //night is between 10pm and 4am (22 -24 and 1 - 4)
 //Make sure your ranges are inclusive
 
 function timeOfDayGreeting(hour) {
-    // you code below
+    if (hour >= 5 && hour <= 11) {
+        return 'good morning'
+    } else if (hour > 11 && hour <= 17) {
+        return 'good afternoon'
+    } else if (hour > 17 && hour <= 21) {
+        return 'good evening'
+    } else if (hour > 21 && hour <= 24) {
+        return 'good night'
+    } else (hour >= 0 && hour < 4); {
+        return 'good night'
+    }
 
 }
 
 //4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
 function isFever(temp) {
-
+    if (temp >= 103) {
+        return 'fever go to hospital'
+    } else if (temp > 98.6) {
+        return 'fever'
+    } else { return 'no fever' }
 }
 
 //5. Write a function that takes in a car object, if it is not moving then return true
@@ -47,7 +75,11 @@ let myCar = {
 }
 
 function isStopped(car) {
-
+    if (car.moving == false) {
+        return true
+    } else {
+        return false
+    }
 }
 
 //6. Write a function that returns true if a dish is yours and is dirty, or false if one of the statements is false
@@ -58,5 +90,8 @@ let dish = {
 }
 
 function washDish(dish) {
+    if (dish.yourDish == true && dish.isDirty == true) {
+        return true
+    } else { return false }
 
 }
