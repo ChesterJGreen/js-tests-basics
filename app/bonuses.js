96f13f633f4cf4f42eb59fd5690b215384c24e98
cjgreenie <seejarom@gmail.com>
2021-06-28 21:44:03 -0600

    good

--- app/bonuses.js
+++ app/bonuses.js
@@ -2,5 +2,13 @@
 let str = "team number 2"
 
 function camelCase(str) {
-
+  return str
+    .replace(/\s(.)/g, function (a) {
+      return a.toUpperCase();
+    })
+    .replace(/\s/g, '')
+    .replace(/^(.)/, function (b) {
+      return b.toLowerCase();
+    });
 }
+*// https://www.geeksforgeeks.org/how-to-convert-string-to-camel-case-in-javascript/ //*
